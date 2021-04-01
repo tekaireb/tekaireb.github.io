0210e63e4df2d6e5cac182e3124f5f9efd24e4bb
Tyler Ekaireb <tylerekaireb@gmail.com>
2021-04-01 11:37:31 -0700

    Optimized project page for mobile, enabled hiding projects and added images, added icons for project links

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/ProjectsPage.css';
+
+import github from '../img/github.svg';
+import link from '../img/external_link.svg';
 
 function ProjectsListItem(props) {
     let project = props.project;
@@ -8,15 +11,21 @@ function ProjectsListItem(props) {
 
     return (
         <div className='project_item'>
-            <div className='project_item_header' onClick={() => set_open(!open)}>
-                <div data-title="Title" className={'project_item_title ' + (open ? 'open' : '')}>{project.title}</div>
-                <div data-title="Year">{project.start_date.slice(-4)}</div> {/* Date format: {MONTH} {DAY}, {YEAR} */}
-                <div data-title="Links">
-                    <a href={project.link}>LINK</a>
+            <div className={'project_item_header ' + (open ? 'open' : '')} onClick={() => set_open(!open)}>
+                <div data-title="Title" className='project_item_title'>
+                    {project.title}
+                </div>
+                <div data-title="Year" className='project_item_year'>
+                    {project.end_date.slice(-4)} {/* Date format: {MONTH} {DAY}, {YEAR} */}
+                </div>
+                <div data-title="Links" className='project_item_links'>
+                    {project.github && <a href={project.github} target='_blank' rel="noreferrer"><img src={github} alt='GitHub Link' className='icon' /></a>}
+                    {project.link && <a href={project.link} target='_blank' rel="noreferrer"><img src={link} alt='External Link' className='icon' /></a>}
                 </div>
             </div>
             <div className={'project_item_content ' + (open ? 'open' : '')}>
                 {project.description.map(line => <span>{line}<br /></span>)}
+                {project.images && project.images.map(i => <img src={i} alt={project.title + ' media'} className='project_item_image' />)}
             </div>
         </div>
     );
@@ -25,20 +34,7 @@ function ProjectsListItem(props) {
 export default function ProjectsList(props) {
     return (
         <div className='projects_list'>
-            {props.projects.map(project => <ProjectsListItem project={project} />
-                // <div className='project_item'>
-                //     <div className='project_item_header'>
-                //         <div data-title="Title" className='project_item_title'>{project.title}</div>
-                //         <div data-title="Year">{project.start_date.slice(-4)}</div> {/* Date format: {MONTH} {DAY}, {YEAR} */}
-                //         <div data-title="Links">
-                //             <a href={project.link}>LINK</a>
-                //         </div>
-                //     </div>
-                //     <div className='project_item_content'>
-
-                //     </div>
-                // </div>
-            )}
+            {props.projects.map(project => <ProjectsListItem project={project} />)}
         </div>
     );
 }
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -8,7 +8,7 @@ export default function Projects(props) {
             <h1>Projects</h1>
             <h3 className='projects_subheader'><code><a href='/projects'>view more projects</a></code></h3>
             <div className='projects_grid'>
-                {props.projects.map(project => <Project project={project} />)}
+                {props.projects.filter(p => p.show).map(project => <Project project={project} />)}
             </div>
         </div>
     );
